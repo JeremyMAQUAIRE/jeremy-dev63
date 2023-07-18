28100272e18212f73daf19763158c06e6325b771
JeremyMAQUAIRE <134293073+JeremyMAQUAIRE@users.noreply.github.com>
2023-07-18 11:50:19 +0200

    Ajout Matomo + redirection https

--- index.php
+++ index.php
@@ -26,6 +26,23 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- Matomo -->
+  <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="https://supportdev63.matomo.cloud/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '2']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src='//cdn.matomo.cloud/supportdev63.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script> 
+  <!-- End Matomo Code -->
+  
 </head>
 
 <body>
